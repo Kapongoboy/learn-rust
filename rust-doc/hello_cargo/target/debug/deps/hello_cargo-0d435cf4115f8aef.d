36fbc6e2a108716c577423a1cc2fdc7d5e5f7741
Kapongoboy <plamedi.luis11@gmail.com>
2023-02-08 21:53:10 +0200

    rust first day learning done

---
+++ rust-doc/hello_cargo/target/debug/deps/hello_cargo-0d435cf4115f8aef.d
@@ -0,0 +1,5 @@
+C:\repos\learn-rust\rust-doc\hello_cargo\target\debug\deps\hello_cargo-0d435cf4115f8aef.rmeta: src\main.rs
+
+C:\repos\learn-rust\rust-doc\hello_cargo\target\debug\deps\hello_cargo-0d435cf4115f8aef.d: src\main.rs
+
+src\main.rs:
---
+++ rust-doc/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\repos\learn-rust\rust-doc\hello_cargo\target\debug\hello_cargo.exe: C:\repos\learn-rust\rust-doc\hello_cargo\src\main.rs
