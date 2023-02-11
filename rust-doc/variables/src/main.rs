6437a8718bad71a85d738e6389650d0fe23571bc
Kapongoboy <plamedi.luis11@gmail.com>
2023-02-11 20:32:46 +0200

    variables and mutability sub section complete

--- rust-doc/variables/src/main.rs
+++ rust-doc/variables/src/main.rs
@@ -1,6 +1,22 @@
+fn variables_and_mutability() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x*2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+}
+
+fn data_types() {
+    let guess: u32 = "42".parse().expect("Not a number!");
+}
+
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    variables_and_mutability();
+    data_types();
 }
