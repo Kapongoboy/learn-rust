e34863798e26a786620f251bec6011cb2d872b64
Plamedi Luis <plamedi.luis11@gmail.com>
2023-03-09 16:48:41 +0200

    understanding ownership started

---
+++ rust-doc/understanding_ownership/target/debug/deps/understanding_ownership-0e45d98b834b5f7d.d
@@ -0,0 +1,5 @@
+/home/plamedi/repos/learn-rust/rust-doc/understanding_ownership/target/debug/deps/understanding_ownership-0e45d98b834b5f7d.rmeta: src/main.rs
+
+/home/plamedi/repos/learn-rust/rust-doc/understanding_ownership/target/debug/deps/understanding_ownership-0e45d98b834b5f7d.d: src/main.rs
+
+src/main.rs:
---
+++ rust-doc/understanding_ownership/target/debug/deps/understanding_ownership-91a5017b83b791c3.d
@@ -0,0 +1,5 @@
+/home/plamedi/repos/learn-rust/rust-doc/understanding_ownership/target/debug/deps/understanding_ownership-91a5017b83b791c3.rmeta: src/main.rs
+
+/home/plamedi/repos/learn-rust/rust-doc/understanding_ownership/target/debug/deps/understanding_ownership-91a5017b83b791c3.d: src/main.rs
+
+src/main.rs:
